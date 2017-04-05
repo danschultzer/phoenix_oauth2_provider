f46c19bcb44e34ac75d71197d864dd2178912de3
Dan Schultzer <dan@dreamconception.com>
2017-04-04 17:11:37 -0700

    Test client_credentials

--- test/controllers/token_controller_test.exs
+++ test/controllers/token_controller_test.exs
@@ -11,6 +11,11 @@ defmodule PhoenixOauth2Provider.TokenControllerTest do
       redirect_uri: app_redirect_uri,
       code: access_grant_token}
   end
+  def valid_request(%{uid: app_uid, secret: app_secret, redirect_uri: app_redirect_uri}) do
+    %{client_id: app_uid,
+      client_secret: app_secret,
+      grant_type: "client_credentials"}
+  end
 
   def last_access_token do
     ExOauth2Provider.repo.one(from x in ExOauth2Provider.OauthAccessTokens.OauthAccessToken,
@@ -18,25 +23,41 @@ defmodule PhoenixOauth2Provider.TokenControllerTest do
   end
 
   setup %{conn: conn} do
-    user = fixture(:user)
-    application = fixture(:application, %{user: user})
-    access_grant = fixture(:access_grant, %{user: user, application: application})
-    request = valid_request(application, access_grant)
+    application = fixture(:application, %{user: fixture(:user)})
 
-    conn = assign conn, :current_test_user, user
-
-    {:ok, conn: conn, user: user, application: application, access_grant: access_grant, request: request}
+    {:ok, conn: conn, application: application}
   end
 
-  test "create/2", %{conn: conn, request: request} do
-    conn = post conn, oauth_token_path(conn, :create, request)
-    body = json_response(conn, 200)
-    assert last_access_token() == body["access_token"]
+  describe "as client_credentials strategy" do
+    test "create/2", %{conn: conn, application: application} do
+      request = valid_request(application)
+      conn = post conn, oauth_token_path(conn, :create, request)
+
+      body = json_response(conn, 200)
+      assert last_access_token() == body["access_token"]
+      assert nil == body["refresh_token"]
+    end
   end
 
-  test "create/2 with error", %{conn: conn, request: request} do
-    conn = post conn, oauth_token_path(conn, :create), Map.merge(request, %{redirect_uri: "invalid"})
-    body = json_response(conn, 422)
-    assert "The provided authorization grant is invalid, expired, revoked, does not match the redirection URI used in the authorization request, or was issued to another client." == body["error_description"]
+  describe "as authorization_code strategy" do
+    setup %{conn: conn, application: application} do
+      user = fixture(:user)
+      access_grant = fixture(:access_grant, %{user: user, application: application})
+      request = valid_request(application, access_grant)
+
+      {:ok, conn: conn, user: user, application: application, access_grant: access_grant, request: request}
+    end
+
+    test "create/2", %{conn: conn, request: request} do
+      conn = post conn, oauth_token_path(conn, :create, request)
+      body = json_response(conn, 200)
+      assert last_access_token() == body["access_token"]
+    end
+
+    test "create/2 with error", %{conn: conn, request: request} do
+      conn = post conn, oauth_token_path(conn, :create), Map.merge(request, %{redirect_uri: "invalid"})
+      body = json_response(conn, 422)
+      assert "The provided authorization grant is invalid, expired, revoked, does not match the redirection URI used in the authorization request, or was issued to another client." == body["error_description"]
+    end
   end
 end
