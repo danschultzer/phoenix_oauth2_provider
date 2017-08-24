758df336d6d207b4d283e3a6c0e73168616d716b
Dan Schultzer <dan@dreamconception.com>
2017-08-24 11:02:24 -0700

    Load config on runtime

--- lib/phoenix_oauth2_provider.ex
+++ lib/phoenix_oauth2_provider.ex
@@ -12,21 +12,27 @@ defmodule PhoenixOauth2Provider do
   [ex_oauth2_provider](https://github.com/danschultzer/ex_oauth2_provider)
   library.
   """
-  @config             Application.get_env(:phoenix_oauth2_provider, PhoenixOauth2Provider, [])
-  @module             Keyword.get(@config, :module, Mix.Phoenix.base())
-  @router_helpers     Module.concat([Keyword.get(@config, :router, Module.concat(["#{@module}Web", "Router"])), "Helpers"])
-  @resource_owner_key Keyword.get(@config, :current_resource_owner, :current_user)
-
-  @doc false
-  def router_helpers, do: @router_helpers
 
   @doc """
   Will get current resource owner from endpoint
   """
   def current_resource_owner(conn) do
-    case conn.assigns[@resource_owner_key] do
-      nil            -> raise "Resource owner was not found with :#{@resource_owner_key} assigns"
+    resource_owner_key = Keyword.get(config(), :current_resource_owner, :current_user)
+
+    case conn.assigns[resource_owner_key] do
+      nil            -> raise "Resource owner was not found with :#{resource_owner_key} assigns"
       resource_owner -> resource_owner
     end
+  end
+
+  @doc false
+  def config do
+    Application.get_env(:phoenix_oauth2_provider, PhoenixOauth2Provider, [])
+  end
+
+  @doc false
+  def router_helpers do
+    module = Keyword.get(config(), :module, Mix.Phoenix.base())
+    Module.concat([Keyword.get(config(), :router, Module.concat(["#{module}Web", "Router"])), "Helpers"])
   end
 end
