6dd04dac863d655ad69632a4bf7dd4fa92a18e2d
Dan Schultzer <dan@dreamconception.com>
2018-07-05 20:42:19 -0700

    Refactor dummy user schema

--- test/support/user.ex
+++ test/support/user.ex
@@ -2,17 +2,17 @@ defmodule PhoenixOauth2Provider.Test.User do
   @moduledoc false
 
   use PhoenixOauth2Provider.Test.Schema
-  import Ecto.Changeset
+  alias ExOauth2Provider.OauthAccessTokens.OauthAccessToken
+  alias Ecto.Changeset
 
   schema "users" do
     field :email, :string
-    has_many :tokens, ExOauth2Provider.OauthAccessTokens.OauthAccessToken, foreign_key: :resource_owner_id
+    has_many :tokens, OauthAccessToken, foreign_key: :resource_owner_id
 
     timestamps()
   end
 
   def changeset(struct, params \\ %{}) do
-    struct
-    |> cast(params, [:email])
+    Changeset.cast(struct, params, [:email])
   end
 end
