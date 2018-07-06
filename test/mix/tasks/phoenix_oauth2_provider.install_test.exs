681a413fcbbe4043076db3ce325160fb18be9587
Dan Schultzer <dan@dreamconception.com>
2018-07-05 17:06:57 -0700

    Refactor test helpers

--- test/mix/tasks/phoenix_oauth2_provider.install_test.exs
+++ test/mix/tasks/phoenix_oauth2_provider.install_test.exs
@@ -1,8 +1,10 @@
-Code.require_file "../../mix_helpers.exs", __DIR__
+# Get Mix output sent to the current
+# process to avoid polluting tests.
+Mix.shell(Mix.Shell.Process)
 
 defmodule Mix.Tasks.PhoenixOauth2Provider.InstallTest do
   use ExUnit.Case
-  import MixHelper
+  alias PhoenixOauth2Provider.Test.MixHelpers
 
   defmodule MigrationsRepo do
     def __adapter__ do
@@ -21,46 +23,46 @@ defmodule Mix.Tasks.PhoenixOauth2Provider.InstallTest do
   @all_controllers Enum.map(@all_template_dirs -- ~w(layout), &("#{&1}_controller.ex"))
 
   test "generates files for application" do
-    in_tmp "generates_files_for_application", fn ->
+    MixHelpers.in_tmp "generates_files_for_application", fn ->
       ~w(--repo PhoenixOauth2Provider.Test.Repo --log-only --controllers --module PhoenixOauth2Provider.Test --no-provider)
       |> Mix.Tasks.PhoenixOauth2Provider.Install.run()
 
       ~w(application_view.ex authorization_view.ex authorized_application_view.ex phoenix_oauth2_provider_view.ex layout_view.ex phoenix_oauth2_provider_view_helpers.ex)
-      |> assert_file_list(@all_views, web_path("views/phoenix_oauth2_provider/"))
+      |> MixHelpers.assert_file_list(@all_views, web_path("views/phoenix_oauth2_provider/"))
 
       ~w(layout application authorization authorized_application)
-      |> assert_dirs(@all_template_dirs, web_path("templates/phoenix_oauth2_provider/"))
+      |> MixHelpers.assert_dirs(@all_template_dirs, web_path("templates/phoenix_oauth2_provider/"))
 
       ~w(application_controller.ex authorization_controller.ex authorized_application_controller.ex token_controller.ex)
-      |> assert_file_list(@all_controllers, web_path("controllers/phoenix_oauth2_provider/"))
+      |> MixHelpers.assert_file_list(@all_controllers, web_path("controllers/phoenix_oauth2_provider/"))
 
-      assert_file web_path("controllers/phoenix_oauth2_provider/application_controller.ex"), fn file ->
+      MixHelpers.assert_file web_path("controllers/phoenix_oauth2_provider/application_controller.ex"), fn file ->
         assert file =~ "defmodule PhoenixOauth2Provider.Test.PhoenixOauth2Provider.ApplicationController do"
       end
     end
   end
 
   test "does not generate files for full" do
-    in_tmp "does_not_generate_files_for_full", fn ->
+    MixHelpers.in_tmp "does_not_generate_files_for_full", fn ->
       ~w(--repo PhoenixOauth2Provider.Test.Repo --full --log-only --no-boilerplate --no-provider)
       |> Mix.Tasks.PhoenixOauth2Provider.Install.run()
 
-      assert_file_list([], @all_views, web_path("views/phoenix_oauth2_provider/"))
+      MixHelpers.assert_file_list([], @all_views, web_path("views/phoenix_oauth2_provider/"))
 
-      assert_dirs([], @all_template_dirs, web_path("templates/phoenix_oauth2_provider/"))
+      MixHelpers.assert_dirs([], @all_template_dirs, web_path("templates/phoenix_oauth2_provider/"))
 
-      assert_file_list([], @all_controllers, web_path("controllers/phoenix_oauth2_provider/"))
+      MixHelpers.assert_file_list([], @all_controllers, web_path("controllers/phoenix_oauth2_provider/"))
     end
   end
 
   test "updates config" do
-    in_tmp "installs_phoenix_oauth2_provider_config", fn ->
+    MixHelpers.in_tmp "installs_phoenix_oauth2_provider_config", fn ->
       file_path = "config.exs"
-      File.touch! file_path
+      File.touch!(file_path)
       ~w(--repo PhoenixOauth2Provider.Test.Repo --no-boilerplate --no-migrations --config-file #{File.cwd!}/#{file_path})
       |> Mix.Tasks.PhoenixOauth2Provider.Install.run()
 
-      assert_file file_path, fn file ->
+      MixHelpers.assert_file file_path, fn file ->
         assert file =~ "config :phoenix_oauth2_provider, PhoenixOauth2Provider"
         assert file =~ "module: PhoenixOauth2Provider"
         assert file =~ "current_resource_owner: :current_user"
@@ -74,7 +76,7 @@ defmodule Mix.Tasks.PhoenixOauth2Provider.InstallTest do
   end
 
   test "instructions" do
-    in_tmp "prints_instructions", fn ->
+    MixHelpers.in_tmp "prints_instructions", fn ->
       ~w(--repo PhoenixOauth2Provider.Test.Repo --no-boilerplate --no-migrations --no-config)
       |> Mix.Tasks.PhoenixOauth2Provider.Install.run()
 
@@ -180,26 +182,6 @@ defmodule Mix.Tasks.PhoenixOauth2Provider.InstallTest do
       |>  Mix.Tasks.PhoenixOauth2Provider.Install.run()
 
       assert_received {:mix_shell, :info, ["mix phoenix_oauth2_provider.install --application"]}
-    end
-  end
-
-  defp assert_dirs(dirs, full_dirs, path) do
-    Enum.each dirs, fn dir ->
-      assert File.dir?(Path.join(path, dir))
-    end
-
-    Enum.each full_dirs -- dirs, fn dir ->
-      refute File.dir?(Path.join(path, dir))
-    end
-  end
-
-  defp assert_file_list(files, full_files, path) do
-    Enum.each files, fn file ->
-      assert_file Path.join(path, file)
-    end
-
-    Enum.each full_files -- files, fn file ->
-      refute_file Path.join(path, file)
     end
   end
 
